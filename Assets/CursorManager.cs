887768dd52b6c2a3dd3939fca3ad6706bb81b3da
vctrnl <victurnalphantom@gmail.com>
2025-05-06 13:08:48 +0700

    try push

--- Assets/CursorManager.cs
+++ Assets/CursorManager.cs
@@ -5,16 +5,16 @@ public class CursorManager : MonoBehaviour
     void Start()
     {
         // Mengubah cursor menjadi custom
-        Cursor.SetCursor(Resources.Load<Texture2D>("cursor"), Vector2.zero, CursorMode.Auto);
+        // Cursor.SetCursor(Resources.Load<Texture2D>("cursor"), Vector2.zero, CursorMode.Auto);
     }
 
     void Update()
     {
         // Mengatur visibilitas cursor
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.visible = !Cursor.visible;
-        }
+        // if (Input.GetKeyDown(KeyCode.Escape))
+        // {
+        //     Cursor.visible = !Cursor.visible;
+        // }
     }
 }
     
--- Assets/CustomCursor.cs
+++ Assets/CustomCursor.cs
@@ -5,8 +5,8 @@ public class CustomCursor : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Get the current mouse position in world coordinatest 
-        transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y); // Get the current mouse position
+        // Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Get the current mouse position in world coordinatest 
+        // transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y); // Get the current mouse position
     }    
 
     // Update is called once per frame
